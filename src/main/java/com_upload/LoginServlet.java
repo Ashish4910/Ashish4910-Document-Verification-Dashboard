7b7d744659904d28f6e782e8d012f994ae188fbe
Vinayak More <morevinayak1998@gmail.com>
2024-02-21 11:28:13 +0530

    Header Change demo

--- src/main/java/com_upload/LoginServlet.java
+++ src/main/java/com_upload/LoginServlet.java
@@ -16,52 +16,50 @@ import Util.Method_Util;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		String uemail = request.getParameter("ntid");
-		String upwd = request.getParameter("passwords");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String uemail = request.getParameter("ntid");
+        String upwd = request.getParameter("passwords");
 
-		HttpSession session = request.getSession();
-		if (uemail.isEmpty() || upwd.isEmpty()) {
-			Method_Util.showErrorMessage(response, "Login failed. Please check your email and password.", "Login.jsp");
-			return;
-		}
+        HttpSession session = request.getSession();
+        if (uemail.isEmpty() || upwd.isEmpty()) {
+        	Method_Util.showErrorMessage(response, "Login failed. Please check your email and password.", "Login.jsp");
+            return;
+        }
 
-		Connection con = null;
-		try {
-			con = DatabaseUtil.getConnection();
+        Connection con = null;
+        try {
+            con = DatabaseUtil.getConnection();
+            PreparedStatement pstmt1 = con.prepareStatement(QueryUtil.LoginServlet_SELECT_USER_ACCESS_QUERY);
+            pstmt1.setString(1, uemail);
+            pstmt1.setString(2, upwd);
 
-			PreparedStatement pstmt1 = con.prepareStatement(QueryUtil.LoginServlet_SELECT_USER_ACCESS_QUERY);
-			pstmt1.setString(1, uemail);
-			ResultSet rs = pstmt1.executeQuery();
+            ResultSet rs = pstmt1.executeQuery();
 
-			if (rs.next()) {
-				String ntidFromDatabase = rs.getString("ntid");
-				session.setAttribute("ntidFromDatabase", ntidFromDatabase);
-				response.sendRedirect("Dashboard.jsp");
-			}
+            if (rs.next()) {
+                String ntidFromDatabase = rs.getString("ntid");
+                session.setAttribute("ntidFromDatabase", ntidFromDatabase);
+                response.sendRedirect("Dashboard.jsp");
+            }
 
-			if (Method_Util.isValidUser(uemail, upwd)) {
-
-				if (Method_Util.isDuplicateEntry(con, uemail)) {
-					session.setAttribute("uemail", uemail);
-					Method_Util.showSuccessMessage(response, "Login successful. Welcome! ", "Dashboard.jsp");
-				} else {
-					session.setAttribute("uemail", uemail);
-					Method_Util.executeUpdateQuery(con, QueryUtil.LoginServlet_INSERT_USER_QUERY, uemail, upwd);
-
-					Method_Util.showSuccessMessage(response, "Login successful. Welcome! ", "Dashboard.jsp");
-				}
-			} else {
-				Method_Util.showErrorMessage(response, "Login failed. Please check your email and password.",
-						"Login.jsp");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			DatabaseUtil.closeConnection(con);
-		}
-	}
+            if (Method_Util.isValidUser(uemail, upwd)) {
+                if (Method_Util.isDuplicateEntry(con, uemail)) {
+                    session.setAttribute("uemail", uemail);
+                    Method_Util.showSuccessMessage(response, "Login successful. Welcome!", "Dashboard.jsp");
+                } else {
+                	session.setAttribute("uemail", uemail);
+                	Method_Util.executeUpdateQuery(con, QueryUtil.LoginServlet_INSERT_USER_QUERY, uemail, upwd);
+                	Method_Util.showSuccessMessage(response, "Login successful. Welcome!", "Dashboard.jsp");
+                }
+            } else {
+            	Method_Util.showErrorMessage(response, "Login failed. Please check your email and password.", "Login.jsp");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DatabaseUtil.closeConnection(con);
+        }
+    }
 }
